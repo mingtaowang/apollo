e0798b831b9834aa8e13efc098aa2bcae3383fb2
wangmingtao <wangmingtao@peiwo.cn>
2019-06-24 15:52:26 +0800

    fix: show data

--- apollo/views/api/east/items.py
+++ apollo/views/api/east/items.py
@@ -67,22 +67,23 @@ def get_useful_items():
     content = content[content.index('['): content.index(']') + 1]
     content = json.loads(content)
     data = []
+    db.snowball.easts.remove({})
     for item in content:
         temp = item.split(',')
         data.append({
             'code': temp[0],  # 代码
             'name': temp[1],  # name
             'day': temp[3],  # day
-            'per_value': temp[4],  # 单位净值
-            'one_day': temp[6],  # 日增长率
-            'week': temp[7],  # 近一周
-            'month': temp[8],  # 近一月
-            'three_month': temp[9],  # 近三月
-            'six_month': temp[10],  # 近六月
-            'year': temp[11],  # 近一年
-            'two_year': temp[12],  # 近两年
-            'three_year': temp[13],  # 近三年
-            'total': temp[14]  # 成立以来
+            'per_value': round(float(temp[4]), 3) if temp[4] else None,  # 单位净值
+            'one_day': round(float(temp[6]), 3) if temp[6] else None,  # 日增长率
+            'week': round(float(temp[7]), 3) if temp[7] else None,  # 近一周
+            'month': round(float(temp[8]), 3) if temp[8] else None,  # 近一月
+            'three_month': round(float(temp[9]), 3) if temp[9] else None,  # 近三月
+            'six_month': round(float(temp[10]), 3) if temp[10] else None,  # 近六月
+            'year': round(float(temp[11]), 3) if temp[11] else None,  # 近一年
+            'two_year': round(float(temp[12]), 3) if temp[12] else None,  # 近两年
+            'three_year': round(float(temp[13]), 3) if temp[13] else None,  # 近三年
+            'total': round(float(temp[14]), 3) if temp[14] else None  # 成立以来
         })
     db.snowball.easts.insert(data)
     return render_template('east/quality.html')
@@ -90,29 +91,27 @@ def get_useful_items():
 
 @bp.route('/statistics')
 def show_items():
-    # per_values = db.snowball.easts.find().limit(30).sort('per_value', -1)
-    # total_values = db.snowball.easts.find().limit(30).sort('total_value', -1)
-
     data = {}
-    fields = ['one_day', 'week', 'month', 'three_month', 'six_month', 'year', 'two_year', 'three_year', 'total']
+    fields = ['one_day', 'week', 'month', 'three_month', 'six_month', 'year', 'two_year', 'three_year',
+              'total']
     for field in fields:
         items = db.snowball.easts.find().limit(30).sort(field, -1)
         data = get_color(data, items, fields, sort=field)
     return_list = []
     for key, value in data.items():
         return_list.append(value)
-    return_list.sort(key=lambda l: (l["one_day"], l["week"]), reverse=True)
+    return_list.sort(key=lambda l: l["one_day"], reverse=True)
     return render_template('east/statistics.html', data=return_list)
 
 
 def get_color(data, items, fields, sort='one_day'):
-    colors = {field: 0 for field in fields}
     for index, item in enumerate(items):
+        colors = {field: 0 for field in fields}
         default_data = data.setdefault(item.get('code'), item)
         default_color = default_data.setdefault('color', colors)
         if index < 10:
             default_color[sort] = 1
-        elif 10 <= index < 19:
+        elif 10 <= index < 20:
             default_color[sort] = 2
         else:
             default_color[sort] = 3
